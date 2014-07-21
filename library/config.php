48b3f81ffb98fe2439a1d799910349e81aa2b427
Ryan Schie <rjschie@gmail.com>
2014-07-21 08:17:01 +0000

    mysql config updated

--- library/config.php
+++ library/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'app_listsapp');
-define('DB_USER', 'root');
-define('DB_PASS', 'rschie');
+define('DB_HOST', 'internal-db.s192738.gridserver.com');
+define('DB_NAME', 'db192738_app_listsapp');
+define('DB_USER', 'db192738');
+define('DB_PASS', '0ziQf8n6$@#62d');
